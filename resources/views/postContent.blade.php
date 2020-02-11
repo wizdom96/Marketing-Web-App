2e7dba4ae85409f190e1950f896416de4bc3990b
Aleksandar <aleksandar96vasileski@gmail.com>
2020-02-11 12:27:05 +0100

    add content table

--- resources/views/postContent.blade.php
+++ resources/views/postContent.blade.php
@@ -84,7 +84,50 @@
                             <label for="password_again" class="col-md-4 col-form-label text-md-right">{{ __('Year') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password_again" type="text" name="year">
+                            <select name="transmission" id="transmission">
+                                <option value="0">Select Year</option>
+                                <option value="2020">2020</option>
+                                <option value="2019">2019</option>
+                                <option value="2018">2018</option>
+                                <option value="2017">2017</option>
+                                <option value="2016">2016</option>
+                                <option value="2015">2015</option>
+                                <option value="2014">2014</option>
+                                <option value="2013">2013</option>
+                                <option value="2012">2012</option>
+                                <option value="2011">2011</option>
+                                <option value="2010">2010</option>
+                                <option value="2009">2009</option>
+                                <option value="2008">2008</option>
+                                <option value="2007">2007</option>
+                                <option value="2006">2006</option>
+                                <option value="2005">2005</option>
+                                <option value="2004">2004</option>
+                                <option value="2003">2003</option>
+                                <option value="2002">2002</option>
+                                <option value="2001">2001</option>
+                                <option value="2000">2000</option>
+                                <option value="1999">1999</option>
+                                <option value="1998">1998</option>
+                                <option value="1997">1997</option>
+                                <option value="1996">1996</option>
+                                <option value="1995">1995</option>
+                                <option value="1994">1994</option>
+                                <option value="1993">1993</option>
+                                <option value="1992">1992</option>
+                                <option value="1991">1991</option>
+                                <option value="1990">1990</option>
+                                <option value="1989">1989</option>
+                                <option value="1988">1988</option>
+                                <option value="1987">1987</option>
+                                <option value="1986">1986</option>
+                                <option value="1985">1985</option>
+                                <option value="1984">1984</option>
+                                <option value="1983">1983</option>
+                                <option value="1982">1982</option>
+                                <option value="1981">1981</option>
+                                <option value="1980">1980</option>
+                                </select>
                             </div>
                         </div>
 
@@ -108,7 +151,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Price') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="number"  name="price" required>
+                                <input id="password" type="text"  name="price" required>
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -138,13 +181,12 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Transmission') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="text"  name="transmission" required>
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <select name="transmission" id="transmission">
+                                <option value="0">Select transmission</option>
+                                <option value="automatic">Automatic</option>
+                                <option value="manual">Manual</option>
+                                <option value="tiptronic">Steptronic/Tiptronic</option>
+                                </select>
                             </div>
                         </div>
 
@@ -153,13 +195,14 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Fuel') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="text"  name="fuel" required>
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <select name="fuel" id="fuel">
+                                <option value="0">Fuel Type</option>
+                                <option value="petrol">Petrol</option>
+                                <option value="diesel">Diesel</option>
+                                <option value="electric">Electric</option>
+                                <option value="hybrid">Hybrid</option>
+                                <option value="lpg">Lpg</option>
+                                </select>
                             </div>
                         </div>
 
@@ -183,13 +226,16 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Type') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="text"  name="type" required>
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <select name="type" id="type">
+                                <option value="0">Select Vehicle Type</option>
+                                <option value="hatchback">Hatchback</option>
+                                <option value="sedan">Sedan</option>
+                                <option value="mpv">MPV</option>
+                                <option value="suv">SUV</option>
+                                <option value="crossover">Crossover</option>
+                                <option value="coupe">Coupe</option>
+                                <option value="convertible">Convertible</option>
+                                </select>
                             </div>
                         </div>
 
